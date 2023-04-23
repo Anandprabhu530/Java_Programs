9c6278d3c5f8c6e460702ff5991a882e66bc557e
Anandprabhu530 <97842562+Anandprabhu530@users.noreply.github.com>
2023-04-23 08:52:47 +0000

    Updated Dynamic_Polymorphism.java

--- OOP_Java/Polymorphism/Dynamic_Polymorphism.java
+++ OOP_Java/Polymorphism/Dynamic_Polymorphism.java
@@ -2,14 +2,13 @@ import java.util.*;
 class Dynamic_Polymorphism{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    System.out.println("With method Overriding: ");
+    System.out.println("With method Overriding...Check github for code.... ");
     Model obj1 = new Samsung();
     obj1.Processor();
     Model obj2 = new Apple();
     obj2.Processor();
     Model obj3 = new Asus();
     obj3.Processor();
-    
   }
 }
 
@@ -21,20 +20,20 @@ class Model{
 
 class Samsung extends Model{
   void Processor(){
-    System.out.println("Samsung with Exynos Processor");
+    System.out.println("Model class with Samsung that has Exynos Processor");
   }
 }
 
 class Apple extends Model{
   @Override
   void Processor(){
-    System.out.println("Apple with A15 Processor");
+    System.out.println("Model class with Apple that has A15 Processor");
   }
 }
 
 class Asus extends Model{
   @Override
   void Processor(){
-    System.out.println("Asus with Snapdragon Processor");
+    System.out.println("Model class with Asus that has Snapdragon Processor");
   }
 }
